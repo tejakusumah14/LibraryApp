25f5095e061730c2d8b3d4f40158bd5322d17f50
tejakusumah14 <kusumahteja6@gmail.com>
2023-05-21 22:37:48 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Teja Kusumah"; // TODO: replace this
+
+const studentId = "FE4321754"; // TODO: replace this
+
+const NetlifyDeployUrl = "TODO: replace me"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
